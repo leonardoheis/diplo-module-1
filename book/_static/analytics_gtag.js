9f35b9bbd26f50c9601236d8280026e6d59a2d23
Leonardo Heis <leonardo.heis@gmail.com>
2026-03-26 00:37:58 -0300

    Update project metadata and Google Analytics configuration to reflect new package name 'diplo-modulo-1'

--- book/_static/analytics_gtag.js
+++ book/_static/analytics_gtag.js
@@ -2,4 +2,4 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
-gtag('config', '(REPLACE WITH Google Analytics Tag)');
+gtag('config', 'diplo-modulo-1 Google Analytics Tag');
