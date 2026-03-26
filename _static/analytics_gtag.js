8f2247ff64616c689e8d0c9e42ccb9579b5cd014
Leonardo Heis <leonardo.heis@gmail.com>
2026-03-25 21:33:31 -0300

    Refactor project metadata by removing parentheses from the package name and Google Analytics tags, and update the repository base URL in configuration files.

--- _static/analytics_gtag.js
+++ _static/analytics_gtag.js
@@ -1,3 +1,3 @@
 window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
-ga('create', '(diplo-modulo-1 Google Analytics Tag)', 'auto');
+ga('create', 'diplo-modulo-1 Google Analytics Tag', 'auto');
 ga('send', 'pageview');
